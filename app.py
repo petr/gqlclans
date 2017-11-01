ff0c38726b4aaf6c78eebf388e8c8eb4a2b3934f
frsv <contact@frsv.me>
2017-11-01 12:17:35 +0300

    Add aiohttp server with grapqhl and graphiql endpoints, update tests

---
+++ app.py
@@ -0,0 +1,19 @@
+
+from aiohttp import web
+from aiohttp_graphql import GraphQLView
+
+from schema import schema
+
+
+app = web.Application()
+app.router.add_get('/', lambda req: web.HTTPTemporaryRedirect(f'/graphiql?{req.query_string}'))
+
+graphiql_view = GraphQLView(schema=schema, graphiql=True)
+app.router.add_route('*', '/graphiql', graphiql_view)
+
+graphql_view = GraphQLView(schema=schema, batch=True)
+app.router.add_route('*', '/graphql', graphql_view)
+
+
+if __name__ == '__main__':
+    web.run_app(app, port=8567)
